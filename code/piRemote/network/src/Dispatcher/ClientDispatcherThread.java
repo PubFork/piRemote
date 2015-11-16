164ed0d4c1b811b505c5c759a17aaa96ba3746a9
fmurer <fmurer@student.ethz.ch>
2015-11-16 13:26:16 +0100

    dispatcher template

--- code/piRemote/network/src/Dispatcher/ClientDispatcherThread.java
+++ code/piRemote/network/src/Dispatcher/ClientDispatcherThread.java
@@ -1,8 +1,10 @@
 package Dispatcher;
 
 import KeepAlive.ClientKeepAliveThread;
+import MessageObject.Message;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
 /**
@@ -14,6 +16,7 @@ public class ClientDispatcherThread extends AbstractDispatcherThread {
     private ClientKeepAliveThread keepAliveThread;
     private Thread clientDispatcher;
     private Socket socket;
+    private ObjectInputStream inputStream;
     // private ClientCore clientCore needed for the mainQueue;
 
     // constructor
@@ -21,13 +24,36 @@ public class ClientDispatcherThread extends AbstractDispatcherThread {
 
         // set keep-alive thread
         keepAliveThread = keepAlive;
+
         // set the socket for the client/server connection
         this.socket = socket;
+
+        try {
+            // set the inputstream of the socket.
+            inputStream = new ObjectInputStream(this.socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // set up the thread
+        clientDispatcher = new Thread(this);
+        clientDispatcher.start();
     }
 
 
     @Override
     public void run() {
 
+        // check keepaliveThread
+
+        try {
+            Message readMessage = (Message) inputStream.readObject();
+
+            // put message on clientcore mainQueue
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
