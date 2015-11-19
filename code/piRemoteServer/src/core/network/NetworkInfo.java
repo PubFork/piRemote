47c0735b23449672784ad2d944017243144daa60
fmurer <fmurer@student.ethz.ch>
2015-11-19 20:53:17 +0100

    read out ip and port from client

--- code/piRemoteServer/src/core/network/NetworkInfo.java
+++ code/piRemoteServer/src/core/network/NetworkInfo.java
@@ -1,15 +1,17 @@
 package core.network;
 
+import java.net.InetAddress;
+
 /**
  * Created by Fabian on 13.11.15.
  */
 public class NetworkInfo {
-    int ip;
+    InetAddress ip;
     int port;
     long lastSeen;
 
 
-    public NetworkInfo(int ip, int port, long lastSeen){
+    public NetworkInfo(InetAddress ip, int port, long lastSeen){
         this.ip = ip;
         this.port = port;
         this.lastSeen = lastSeen;
--- code/piRemoteServer/src/core/network/ServerDispatcherThread.java
+++ code/piRemoteServer/src/core/network/ServerDispatcherThread.java
@@ -7,7 +7,11 @@ import SharedConstants.CoreCsts;
 import com.sun.corba.se.spi.activation.Server;
 import core.ServerCore;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
 import java.util.concurrent.BlockingQueue;
 
@@ -21,6 +25,7 @@ public class ServerDispatcherThread implements Runnable {
     private List<NetworkInfo> morgueQueue = new ArrayList<>();
     private Thread serverDispatcher;
     private ServerSocket serverSocket;
+    private Socket clientSocket;
     private HashMap<UUID, NetworkInfo> sessionTable;
     private BlockingQueue<Message> sendingQueue;
 
@@ -38,29 +43,44 @@ public class ServerDispatcherThread implements Runnable {
     public void run() {
 
         // TODO: How to read from ServerSocket?
-        Message receivedMessage = null; // need to be the received Message!
-        UUID uuid = receivedMessage.getUuid();
-
-        // TODO: check morgeQueue
-
-        // check the sessionTable
-        if (!sessionTable.containsKey(uuid)) {
-            // TODO: how to get ip and port from client?
-            NetworkInfo clientInfo = new NetworkInfo(1234, 1234, System.currentTimeMillis());
-            sessionTable.put(uuid, clientInfo);
-        } else {
-            // update lastSeen
-            sessionTable.get(uuid).lastSeen = System.currentTimeMillis();
-        }
-
+        // Message receivedMessage = null; // need to be the received Message!
 
         try {
+            clientSocket = serverSocket.accept();
+            InetAddress ip = clientSocket.getInetAddress();
+            int port = clientSocket.getPort();
+
+            ObjectInputStream input = new ObjectInputStream(clientSocket.getInputStream());
+
+            // TODO: what about first connection message? it is not of type Message! or yes?
+            Message receivedMessage = (Message) input.readObject();
+            UUID uuid = receivedMessage.getUuid();
+
+            // TODO: check morgeQueue
+
+            // check the sessionTable
+            if (!sessionTable.containsKey(uuid)) {
+                NetworkInfo clientInfo = new NetworkInfo(ip, port, System.currentTimeMillis());
+                sessionTable.put(uuid, clientInfo);
+            } else {
+                // update lastSeen
+                sessionTable.get(uuid).lastSeen = System.currentTimeMillis();
+            }
+
+
+
             // TODO: first check if it is a FilePickerRequest
 
             // put message on mainQueue from Core
             ServerCore.mainQueue.put(receivedMessage);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
     }
 }
