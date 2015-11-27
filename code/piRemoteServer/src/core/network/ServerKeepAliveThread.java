c82f8ed15c6a12e42f3ef8bb64556fdcb9300d46
Mickey Vänskä <mvaenskae@gmail.com>
2015-11-28 00:57:39 +0100

    ServerNetwork and ServerKeepAliveThread refactored and documented.
    Signed-off-by: Mickey Vänskä <mvaenskae@gmail.com>

--- code/piRemoteServer/src/core/network/ServerKeepAliveThread.java
+++ code/piRemoteServer/src/core/network/ServerKeepAliveThread.java
@@ -8,41 +8,68 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+
 /**
  * created by fabian on 13.11.15
  */
 
 public class ServerKeepAliveThread implements Runnable {
 
-    private ServerDispatcherThread dispatcherThread;
-    private HashMap<UUID,NetworkInfo> sessionTable;
-    private Thread keepAliveThread;
+    private final ServerDispatcherThread dispatcherThread;
+    private final ServerSenderThread senderThread;
+    private final HashMap<UUID, NetworkInfo> sessionTable;
+    private final Thread keepAliveThread;
 
-    private long INTERVAL = 1000;
+    private final long INTERVAL = 1000;
+    private final long ALLOWED_DROPS = 3;
+    private final long TIMEOUT = ALLOWED_DROPS * INTERVAL;
 
-    public ServerKeepAliveThread(ServerDispatcherThread dispatcherThread, HashMap sessionTable) {
+    /**
+     * Default constructor for the KeepAliveThread on the Server.
+     * @param dispatcherThread DispatcherThread of the server, required for accessing the morgueQueue
+     * @param sessionTable
+     */
+    public ServerKeepAliveThread(ServerDispatcherThread dispatcherThread, ServerSenderThread senderThread, HashMap<UUID, NetworkInfo> sessionTable) {
         this.dispatcherThread = dispatcherThread;
+        this.senderThread = senderThread;
         this.sessionTable = sessionTable;
 
         keepAliveThread = new Thread(this);
-        keepAliveThread.start();
+        //keepAliveThread.start();
     }
 
     @Override
     public void run() {
         while (ServerNetwork.isRunning()) {
             for (Map.Entry<UUID, NetworkInfo> entry : sessionTable.entrySet()) {
-                if (System.currentTimeMillis() - entry.getValue().lastSeen < 3 * INTERVAL) {
-                    ServerSenderThread.getSendingQueue().add(new Message(entry.getKey(), ServerCore.getState().getServerState(), ServerCore.getState().getApplicationState()));
+                // Iterate over all elements of the sessionTable
+                long stillAlive = System.currentTimeMillis() - entry.getValue().getLastSeen();
+                if (stillAlive < TIMEOUT) {
+                    // If the server<->client link hasn't timed out yet, place a keep alive message on
+                    // the sending queue.
+                    Message keepAlive = new Message(entry.getKey(), ServerCore.getState().getServerState(), ServerCore.getState().getApplicationState());
+                    senderThread.getQueue().add(keepAlive);
                 } else {
-                    ServerDispatcherThread.getmorgueQueue().add(new Session(entry.getKey(),entry.getValue()));
+                    // Else set the session to be trashed.
+                    Session deadSession = new Session(entry.getKey(), entry.getValue());
+                    dispatcherThread.getQueue().add(deadSession);
                 }
             }
+
+            // Wait INTERVAL time before checking for a need to resend a keep-alive.
             try {
-                wait(INTERVAL);
+                Thread.sleep(INTERVAL);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * Returns direct reference to the keepAliveThread.
+     * @return Direct reference to keepAliveThread.
+     */
+    public Thread getThread() {
+        return keepAliveThread;
     }
 }
--- code/piRemoteServer/src/core/network/ServerNetwork.java
+++ code/piRemoteServer/src/core/network/ServerNetwork.java
@@ -2,70 +2,116 @@ package core.network;
 
 import MessageObject.Message;
 
-import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by Fabian on 13.11.15.
  */
-public class ServerNetwork {
+public class ServerNetwork implements Runnable {
 
     private ServerDispatcherThread dispatcherThread;
     private ServerKeepAliveThread keepAliveThread;
     private ServerSenderThread senderThread;
-    private HashMap<UUID, NetworkInfo> sessionTable = new HashMap<>();
 
-    private static boolean serverRunning;
+    private static AtomicBoolean running;
+    private DatagramSocket socket;
+
+    private final Thread networkThread;
+    private final int port;
+    private final HashMap<UUID, NetworkInfo> sessionTable;
 
     /**
-     * Need to be called by the ServerCore to create the network part
-     * @param port is needed to create the ServerSocket
+     * Default constructor for creating the ServerNetwork and all its components.
+     * @param port Port on which the server communicates.
      */
     public ServerNetwork(int port) {
-        serverRunning = true;
-        dispatcherThread = new ServerDispatcherThread(port, sessionTable, senderThread);
-        senderThread = new ServerSenderThread(sessionTable);
-        keepAliveThread = new ServerKeepAliveThread(dispatcherThread, sessionTable);
+        this.port = port;
+
+        this.sessionTable = new HashMap<>();
+        this.running = new AtomicBoolean(false);
+        this.networkThread = new Thread(this);
+        this.networkThread.start();
     }
 
 
+    @Override
+    public void run() {
+        try {
+            socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        running.set(true);
+
+        // Initialise the threads of the network.
+        senderThread = new ServerSenderThread(sessionTable);
+        dispatcherThread = new ServerDispatcherThread(port, sessionTable, senderThread);
+        keepAliveThread = new ServerKeepAliveThread(dispatcherThread, senderThread, sessionTable);
+
+        // Start the threads of the network.
+        senderThread.getThread().start();
+        dispatcherThread.getThread().start();
+        keepAliveThread.getThread().start();
+    }
+
 
     /**
-     * use these getter-functions to get the fields you need.
+     * Returns direct reference of the SenderThread.
+     * @return Direct reference of SenderThread.
      */
-
     public ServerSenderThread getSenderThread() {
         return senderThread;
     }
 
+    /**
+     * Returns direct reference of the SendingQueue if the SenderThread is running.
+     * @return SendingQueue if SenderThread exists, else null.
+     */
+    public BlockingQueue<Message> getSendingQueue(){
+        if(senderThread == null) {
+            return null;
+        }
+        return senderThread.getQueue();
+    }
+
+    /**
+     * Returns direct reference of the SessionTable.
+     * @return Direct reference of SessionTable.
+     */
     public HashMap<UUID, NetworkInfo> getSessionTable() {
         return sessionTable;
     }
 
+    /**
+     * Return direct reference to queue of sessions to be terminated by the server.
+     * @return MorgueQueue if DispatcherThread exists, else null.
+     */
+    public BlockingQueue<Session> getMorgueQueue(){
+        if(dispatcherThread == null) {
+            return null;
+        }
+        return dispatcherThread.getQueue();
+    }
+
+    /**
+     * Returns direct reference of the KeepAliveThread.
+     * @return Direct reference of KeepAliveThread.
+     */
     public ServerKeepAliveThread getKeepAliveThread() {
         return keepAliveThread;
     }
 
-    public BlockingQueue<Message> getSendingQueue(){
-        if(senderThread==null) {
-            return null;
-        }
-        return senderThread.getSendingQueue();
-    }
-
-    public List<Session> getmorgueQueue(){
-        if(dispatcherThread==null) {
-            return null;
-        }
-        return dispatcherThread.getmorgueQueue();
-    }
-
-
+    /**
+     * Return whether ServerNetwork is running or not.
+     * @return Returns true if the ServerNetwork is running, else false.
+     */
     public static boolean isRunning () {
-        return serverRunning;
+        return running.get();
     }
 }
