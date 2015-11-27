db46f2bfa977ec7597c5702426aad760e71ea839
Mickey Vänskä <mvaenskae@gmail.com>
2015-11-27 21:40:49 +0100

    Added documentation to these 2 classes, adjusted coding style.
    Signed-off-by: Mickey Vänskä <mvaenskae@gmail.com>

--- code/piRemoteServer/src/core/network/NetworkInfo.java
+++ code/piRemoteServer/src/core/network/NetworkInfo.java
@@ -1,19 +1,26 @@
 package core.network;
 
 import java.net.InetAddress;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Fabian on 13.11.15.
  */
 public class NetworkInfo {
-    InetAddress ip;
-    int port;
-    long lastSeen;
 
+    private final InetAddress ip;
+    private final int port;
+    private final AtomicLong lastSeen;
 
-    public NetworkInfo(InetAddress ip, int port, long lastSeen){
+    /**
+     * Default constructor for saving a client's connection NetworkInfo.
+     * @param ip IP of active client.
+     * @param port Port with which the client communicates.
+     * @param lastSeen AtomicLong of last communication by the client to the server.
+     */
+    public NetworkInfo(InetAddress ip, int port, AtomicLong lastSeen){
         this.ip = ip;
         this.port = port;
-        this.lastSeen = lastSeen;
+        this.lastSeen = new AtomicLong(lastSeen.get());
     }
 }
