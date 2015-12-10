4f222ab69673aa4da7dcc34caff46c6b0053c026
Sandro Kalbermatter <kalsan@users.noreply.github.com>
2015-12-10 11:14:59 +0100

    added omxplayer support

--- code/piRemoteServer/src/application/VideoApplication.java
+++ code/piRemoteServer/src/application/VideoApplication.java
@@ -3,13 +3,24 @@ package application;
 import SharedConstants.ApplicationCsts;
 import core.AbstractApplication;
 
-import java.io.File;
+import java.io.*;
 import java.util.UUID;
 
 /**
  * Created by sandro on 08.12.15.
+ * Application for playing videos and music (not optimized for music content though)
  */
 public class VideoApplication extends AbstractApplication {
+
+    String pathToPlay = "";
+    ProcessBuilder processBuilder = null;
+    Process omxProcess = null;
+    OutputStream omxStdin = null;
+    InputStream omxStderr = null;
+    InputStream omxStdout = null;
+    BufferedReader omxReader = null;
+    BufferedWriter omxWriter = null;
+
     @Override
     public void onApplicationStart() {
         System.out.println("VideoApplication: Starting up.");
@@ -19,7 +30,7 @@ public class VideoApplication extends AbstractApplication {
     @Override
     public void onApplicationStop() {
         System.out.println("VideoApplication: Will now stop.");
-        // TODO: Stop running process (if any)
+        stopProcess();
     }
 
     @Override
@@ -27,21 +38,21 @@ public class VideoApplication extends AbstractApplication {
         System.out.println("VideoApplication: Shall change state to "+newState.toString());
         if(getApplicationState() == null) return; // First time do nothing
 
-        if(getApplicationState().equals(getApplicationState()))
+        if(getApplicationState().equals(newState))
             System.out.println("VideoApplication: Warning: Ignoring attempt to change to current state: "
                     + getApplicationState().toString());
         if(getApplicationState().equals(ApplicationCsts.VideoApplicationState.VIDEO_STOPPED)){
             if(newState.equals(ApplicationCsts.VideoApplicationState.VIDEO_PLAYING)){
-                // TODO: Start playing
+                startProcess(pathToPlay);
             }else{
                 System.out.println("VideoApplication: Warning: Invalid transition stopped -> paused");
             }
         }else{
             // We are playing or paused
             if(newState.equals(ApplicationCsts.VideoApplicationState.VIDEO_STOPPED)){
-                // TODO: Stop process
+                stopProcess();
             }else{
-                // TODO: Press space in running process
+                sendToProcess(" ");
             }
         }
     }
@@ -49,7 +60,8 @@ public class VideoApplication extends AbstractApplication {
     @Override
     public void onFilePicked(File file, UUID senderUUID) {
         System.out.println("VideoApplication: File picked: "+file.getPath());
-        // TODO!
+        pathToPlay = file.getAbsolutePath();
+        startProcess(pathToPlay);
     }
 
     @Override
@@ -77,16 +89,16 @@ public class VideoApplication extends AbstractApplication {
                         // TODO!
                         break;
                     case ApplicationCsts.VIDEO_SPEED_SLOWER:
-                        // TODO!
+                        sendToProcess("2");
                         break;
                     case ApplicationCsts.VIDEO_SPEED_FASTER:
-                        // TODO!
+                        sendToProcess("1");
                         break;
                     case ApplicationCsts.VIDEO_VOLUME_INCREASE:
-                        // TODO!
+                        sendToProcess("+");
                         break;
                     case ApplicationCsts.VIDEO_VOLUME_DECREASE:
-                        // TODO!
+                        sendToProcess("-");
                         break;
                     default:
                         System.out.println("VideoApplication: Warning: Ignoring invalid value: "+Integer.toString(i));
@@ -106,5 +118,51 @@ public class VideoApplication extends AbstractApplication {
     @Override
     public void onReceiveString(String str, UUID senderUUID) {
 
+    }
+
+    void startProcess(String path){
+        if(omxProcess != null) stopProcess();
+        processBuilder = new ProcessBuilder("/usr/bin/omxplayer", path);
+        processBuilder.redirectErrorStream(true);
+        try {
+            omxProcess = processBuilder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(omxProcess != null) {
+            omxStdin = omxProcess.getOutputStream();
+            omxStderr = omxProcess.getErrorStream();
+            omxStdout = omxProcess.getInputStream();
+
+            omxReader = new BufferedReader(new InputStreamReader(omxStdout));
+            omxWriter = new BufferedWriter(new OutputStreamWriter(omxStdin));
+        }
+    }
+
+    void stopProcess(){
+        if(omxProcess != null){
+            omxProcess.destroy();
+            try {
+                omxProcess.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                try {
+                    omxReader.close();
+                    omxWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        omxProcess = null;
+    }
+
+    void sendToProcess(String what){
+        try {
+            omxWriter.write(what);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
