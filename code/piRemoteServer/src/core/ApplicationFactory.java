9033811758e7b410e21dcf732cf8db9bd1927123
FR4NK-W <wirzf@student.ethz.ch>
2015-12-14 15:05:57 +0100

    Added Radio Pi to App factory

--- code/piRemoteServer/src/core/ApplicationFactory.java
+++ code/piRemoteServer/src/core/ApplicationFactory.java
@@ -12,6 +12,7 @@ public class ApplicationFactory {
     public static AbstractApplication makeApplication(CoreCsts.ServerState applicationToStart){
         if(applicationToStart.equals(CoreCsts.ServerState.TRAFFIC_LIGHT)) return new TrafficLight();
         if(applicationToStart.equals(CoreCsts.ServerState.VIDEO)) return new VideoApplication();
+        if(applicationToStart.equals(CoreCsts.ServerState.RADIO_PI)) return new RadioPi();
         return null;
     }
 }
