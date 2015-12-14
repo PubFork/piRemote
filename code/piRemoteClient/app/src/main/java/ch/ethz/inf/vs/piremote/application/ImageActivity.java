e967a55bd3c9e42a417974dfe1a372310ded6291
fmurer <fmurer@student.ethz.ch>
2015-12-14 16:45:07 +0100

    started implementing ImageActivity + SHARED: added new enum IMAGE

--- code/piRemoteClient/app/src/main/java/ch/ethz/inf/vs/piremote/application/ImageActivity.java
+++ code/piRemoteClient/app/src/main/java/ch/ethz/inf/vs/piremote/application/ImageActivity.java
@@ -2,6 +2,10 @@ package ch.ethz.inf.vs.piremote.application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import SharedConstants.ApplicationCsts;
 import ch.ethz.inf.vs.piremote.R;
@@ -9,10 +13,28 @@ import ch.ethz.inf.vs.piremote.core.AbstractClientActivity;
 
 public class ImageActivity extends AbstractClientActivity {
 
+    private final String DEBUG_TAG = "# ImageApp #";
+    private final String WARN_TAG = "# ImageApp WARN #";
+
+    private TextView pickedFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        // pick file button
+        Button pickButton = (Button) findViewById(R.id.button_pick);
+        pickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(DEBUG_TAG, "Clicked button: " + v.toString());
+                sendInt(ApplicationCsts.IMAGE_PICK_FILE);
+            }
+        });
+
+        // shows picked file
+        pickedFile = (TextView) findViewById(R.id.picked_path);
     }
 
     @Override
@@ -22,17 +44,18 @@ public class ImageActivity extends AbstractClientActivity {
 
     @Override
     protected void onReceiveInt(int i) {
-
+        Log.d(DEBUG_TAG, "Received an int: " + i);
     }
 
     @Override
     protected void onReceiveDouble(double d) {
-
+        Log.d(DEBUG_TAG, "Received an double: " + d);
     }
 
     @Override
     protected void onReceiveString(String str) {
-
+        Log.d(DEBUG_TAG, "Received a string: " + str);
+        pickedFile.setText(str);
     }
 
     @Override
