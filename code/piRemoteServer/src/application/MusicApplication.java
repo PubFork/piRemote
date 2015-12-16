f026765ab7b619b7ba44691d202f58fda0b3ae25
Mickey Vänskä <mvaenskae@gmail.com>
2015-12-16 05:03:04 +0100

    SERVER: First version of music application. Still logs a lot.
    Signed-off-by: Mickey Vänskä <mvaenskae@gmail.com>

--- code/piRemoteServer/src/application/MusicApplication.java
+++ code/piRemoteServer/src/application/MusicApplication.java
@@ -4,10 +4,8 @@ import SharedConstants.ApplicationCsts;
 import SharedConstants.ApplicationCsts.ApplicationState;
 import SharedConstants.ApplicationCsts.MusicApplicationState;
 import core.AbstractApplication;
-import core.ApplicationFactory;
-import javafx.application.Application;
 
-import java.io.File;
+import java.io.*;
 import java.util.UUID;
 
 /**
@@ -17,8 +15,16 @@ public class MusicApplication extends AbstractApplication {
     @Override
     public void onApplicationStart() {
         System.out.println("MusicApplication: Initialised.");
-        // TODO: Get actual state from mpd
-        changeApplicationState(MusicApplicationState.MUSIC_STOPPED);
+
+        // Set the default application state based on the actual current state of mpd.
+        String currentState = executeCommand("mpc", null);
+        if (currentState.contains("playing")) {
+            changeApplicationState(MusicApplicationState.MUSIC_PLAYING);
+        } else if (currentState.contains("paused")) {
+            changeApplicationState(MusicApplicationState.MUSIC_PAUSED);
+        } else {
+            changeApplicationState(MusicApplicationState.MUSIC_STOPPED);
+        }
     }
 
     @Override
@@ -30,7 +36,7 @@ public class MusicApplication extends AbstractApplication {
     public void onApplicationStateChange(ApplicationState newState) {
         if(getApplicationState() != null) {
 
-            System.out.printf("MusicApplication: Changing state from: %s\nTo: %s\n",
+            System.out.printf("MusicApplication: Changing state %s -> %s\n",
                     getApplicationState().toString(), newState.toString());
 
             if (getApplicationState().equals(newState)) {
@@ -64,13 +70,8 @@ public class MusicApplication extends AbstractApplication {
     public void onFilePicked(File file, UUID senderUUID) {
         System.out.printf("MusicApplication: Picked file %s. Requesting close.\n", file.getName());
 
-        // Add a header required by client application for sending it to the correct field.
-        StringBuilder sendFileName = new StringBuilder();
-        sendFileName.append("FILE:");
-        sendFileName.append(file.getName());
-
-        // Send it back to the client.
-        sendString(sendFileName.toString());
+        // Send a headified string back to the client.
+        sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_FILESELECTION, file.getName()));
         closeFilePicker(senderUUID);
     }
 
@@ -82,7 +83,14 @@ public class MusicApplication extends AbstractApplication {
         } else if (getApplicationState().equals(MusicApplicationState.MUSIC_STOPPED)) {
             switch (i) {
                 case ApplicationCsts.MUSIC_PLAY:
+                    // Set the application state to playing and start playing music
                     changeApplicationState(MusicApplicationState.MUSIC_PLAYING);
+                    executeCommand("mpc play", null);
+
+                    // Broadcast to each client the currently playing song's name
+                    String toSend = createMessage(ApplicationCsts.MUSIC_PREFIX_SONG, executeCommand("mpc current", null));
+                    System.out.printf("Sending %s\n", toSend);
+                    sendString(toSend);
                     break;
                 case ApplicationCsts.MUSIC_PAUSE:
                 case ApplicationCsts.MUSIC_STOP:
@@ -90,9 +98,37 @@ public class MusicApplication extends AbstractApplication {
                 case ApplicationCsts.MUSIC_NEXT:
                 case ApplicationCsts.MUSIC_PREV:
                     System.out.println("MusicApplication: Ignoring invalid command/state combination.");
-                    // Do nothing for these operations.
                     break;
-                // else allow setting all the switches
+                case ApplicationCsts.MUSIC_STATUS:
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_PLAYLIST, executeCommand("mpc", null)), senderUUID);
+                    break;
+                case ApplicationCsts.MUSIC_VOLUME_UP:
+                    executeCommand("mpc volume +2", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
+                    break;
+                case ApplicationCsts.MUSIC_VOLUME_DOWN:
+                    executeCommand("mpc volume -2", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
+                    break;
+                case ApplicationCsts.MUSIC_LOOP:
+                    executeCommand("mpc repeat", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
+                    break;
+                case ApplicationCsts.MUSIC_SINGLE:
+                    executeCommand("mpc single", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
+                    break;
+                case ApplicationCsts.MUSIC_SHUFFLE:
+                    executeCommand("mpc random", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
+                    break;
+                case ApplicationCsts.MUSIC_CONSUME:
+                    executeCommand("mpc consume", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
+                    break;
+                case ApplicationCsts.MUSIC_GET_PLAYLIST:
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")), senderUUID);
+                    break;
                 default:
                     System.out.println("MusicApplication: Ignoring invalid command/state combination.");
                     break;
@@ -100,57 +136,63 @@ public class MusicApplication extends AbstractApplication {
         } else {
             switch(i) {
                 case ApplicationCsts.MUSIC_PLAY:
+                    // Set the application state to playing and start playing music
                     changeApplicationState(MusicApplicationState.MUSIC_PLAYING);
+                    executeCommand("mpc play", null);
+
+                    // Broadcast to each client the currently playing song's name
+                    String toSend = createMessage(ApplicationCsts.MUSIC_PREFIX_SONG, executeCommand("mpc current", null));
+                    System.out.printf("Sending %s\n", toSend);
+                    sendString(toSend);
                     break;
                 case ApplicationCsts.MUSIC_PAUSE:
                     changeApplicationState(MusicApplicationState.MUSIC_PAUSED);
+                    executeCommand("mpc pause", null);
                     break;
                 case ApplicationCsts.MUSIC_STOP:
                     changeApplicationState(MusicApplicationState.MUSIC_STOPPED);
+                    executeCommand("mpc stop", null);
                     break;
                 case ApplicationCsts.MUSIC_STATUS:
-                    // exec "mpc"
-                    // send back to UUID
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_PLAYLIST, executeCommand("mpc", null)), senderUUID);
                     break;
                 case ApplicationCsts.MUSIC_GET_CURRENT:
-                    // exec "mpc current"
-                    // send back to UUID
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_SONG, executeCommand("mpc current", null)), senderUUID);
                     break;
                 case ApplicationCsts.MUSIC_NEXT:
-                    // exec "mpc next"
-                    // no explicit broadcast
+                    executeCommand("mpc next", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_SONG, executeCommand("mpc current", null)));
                     break;
                 case ApplicationCsts.MUSIC_PREV:
-                    // exec "mpc previous"
-                    // no explicit broadcast
+                    executeCommand("mpc prev", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_SONG, executeCommand("mpc current", null)));
                     break;
                 case ApplicationCsts.MUSIC_VOLUME_UP:
-                    // exec "mpc volume +2"
-                    // broadcast to all clients
+                    executeCommand("mpc volume +2", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
                     break;
                 case ApplicationCsts.MUSIC_VOLUME_DOWN:
-                    // exec "mpc volume -2"
-                    // broadcast to all clients
+                    executeCommand("mpc volume -2", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
                     break;
                 case ApplicationCsts.MUSIC_LOOP:
-                    // exec "mpc loop"
-                    // broadcast to all clients
+                    executeCommand("mpc repeat", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
                     break;
                 case ApplicationCsts.MUSIC_SINGLE:
-                    // exec "mpc single"
-                    // broadcast to all clients
+                    executeCommand("mpc single", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
                     break;
                 case ApplicationCsts.MUSIC_SHUFFLE:
-                    // exec "mpc shuffle"
-                    // broadcast to all clients
+                    executeCommand("mpc random", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
                     break;
                 case ApplicationCsts.MUSIC_CONSUME:
-                    // exec "mpc consume"
-                    // broadcast to all clients
+                    executeCommand("mpc consume", null);
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_EXTRA, executeCommand("/bin/bash -c", "mpc | tail -n1")));
                     break;
                 case ApplicationCsts.MUSIC_GET_PLAYLIST:
-                    // exec "mpc playlist"
-                    // send back to client
+                    sendString(createMessage(ApplicationCsts.MUSIC_PREFIX_PLAYLIST, executeCommand("mpc playlist", null)), senderUUID);
                     break;
                 default:
                     System.out.printf("MusicApplication: Received unknown value %d\n", i);
@@ -168,5 +210,85 @@ public class MusicApplication extends AbstractApplication {
         System.out.format("MusicApplication: Received string %s from client %s\n", str, senderUUID.toString());
     }
 
+    /**
+     * This method is used to execute a local command on the server and have the output returned.
+     * @param strCommand Command to execute.
+     * @param strParams Command passed as extra, to be appended as is
+     * @return Result of the command.
+     */
+    private String executeCommand(String strCommand, String strParams) {
+        ProcessBuilder processBuilder = new ProcessBuilder(buildCommand(strCommand, strParams));
+        processBuilder.redirectErrorStream(true);
+
+        // Logging of command to exec
+        System.out.printf("Music: Executing command %s\n", processBuilder.command().toString());
+
+        Process mpcProcess;
+        InputStream mpcStdout;
+        BufferedReader mpcReader;
+
+        String result = null;
+
+        try {
+            mpcProcess = processBuilder.start();
+            mpcStdout = mpcProcess.getInputStream();
+            mpcReader = new BufferedReader(new InputStreamReader(mpcStdout));
+            mpcProcess.waitFor();
+
+            StringBuilder resultBuilder = new StringBuilder();
+            while ((result = mpcReader.readLine()) != null) {
+                resultBuilder.append(result);
+                resultBuilder.append("\n");
+            }
+            resultBuilder.deleteCharAt(resultBuilder.length()-1);
+            result = resultBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("Executed command, got back: %s\n", result);
+
+        return result;
+    }
+
+    /**
+     * Internal method to quickly build a string with a header used to set the UI of the client.
+     * @param header Header identifying which parts of the UI are to be set.
+     * @param content Value the UI elements will be set to.
+     * @return Returns a concatenated string.
+     */
+    private String createMessage(String header, String content) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(header);
+        builder.append(content);
+
+        return builder.toString();
+    }
+
+    /**
+     * Internal method returning an array of command-strings that can be processed.
+     * @param command Command that is to be split up.
+     * @param raw Raw command that is to be seen as one string. No splitting.
+     * @return Array of strings.
+     */
+    private String[] buildCommand(String command, String raw) {
+        String[] params = command.split(" ");
+        if (raw == null) {
+            return params;
+        } else {
+            int count = params.length;
+            int index = 0;
+            String[] result = new String[count + 1];
+
+            for (; index < params.length; ++index) {
+                result[index] = params[index];
+            }
+
+            result[index] = raw;
+            return result;
+        }
+    }
 
 }
