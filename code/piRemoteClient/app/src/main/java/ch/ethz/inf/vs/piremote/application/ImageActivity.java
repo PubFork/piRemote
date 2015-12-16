5f4ffe67a66dd2af1a7f0a6da0463b2741c5f688
fmurer <fmurer@student.ethz.ch>
2015-12-16 15:29:08 +0100

    Fragment for ImageApplication

--- code/piRemoteClient/app/src/main/java/ch/ethz/inf/vs/piremote/application/ImageActivity.java
+++ code/piRemoteClient/app/src/main/java/ch/ethz/inf/vs/piremote/application/ImageActivity.java
@@ -11,14 +11,14 @@ import SharedConstants.ApplicationCsts;
 import ch.ethz.inf.vs.piremote.R;
 import ch.ethz.inf.vs.piremote.core.AbstractClientActivity;
 
-public class ImageActivity extends AbstractClientActivity {
+public class ImageActivity extends AbstractClientActivity implements ImageFragment.onClickAction {
 
     private final String DEBUG_TAG = "# ImageApp #";
 
     private TextView pickedFile;
     private View progressView;
     private View imageView;
-    private Button prevButton, nextButton;
+    private ImageFragment imageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +34,6 @@ public class ImageActivity extends AbstractClientActivity {
                 sendInt(ApplicationCsts.IMAGE_PICK_FILE);
             }
         });
-
-        prevButton = (Button) findViewById(R.id.button_prev_image);
-        prevButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendInt(ApplicationCsts.IMAGE_PREV);
-            }
-        });
-        nextButton = (Button) findViewById(R.id.button_next_image);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendInt(ApplicationCsts.IMAGE_NEXT);
-            }
-        });
         
         // shows picked file
         pickedFile = (TextView) findViewById(R.id.picked_path);
@@ -56,11 +41,15 @@ public class ImageActivity extends AbstractClientActivity {
         // spinning wheel
         progressView = findViewById(R.id.view_progress);
         imageView = findViewById(R.id.view_image);
+
+        // Fragment
+        imageFragment = new ImageFragment();
+        imageFragment.setArguments(getIntent().getExtras());
     }
 
     @Override
     protected void onApplicationStateChange(ApplicationCsts.ApplicationState newState) {
-
+        updateImageState((ApplicationCsts.ImageApplicationState) newState);
     }
 
     @Override
@@ -83,5 +72,24 @@ public class ImageActivity extends AbstractClientActivity {
     protected void showProgress(boolean show) {
         /*progressView.setVisibility(show ? View.VISIBLE : View.GONE);
         imageView.setVisibility(show ? View.GONE : View.VISIBLE);*/
+    }
+
+    @Override
+    public void onButtonPressed(int state){
+        sendInt(state);
+    }
+
+    public void updateImageState(ApplicationCsts.ImageApplicationState newState) {
+        if (newState != null) {
+            switch (newState) {
+                case IMAGE_DISPLAYED:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, imageFragment).commit();
+                    break;
+                case IMAGE_NOT_DISPLAYED:
+                    getSupportFragmentManager().beginTransaction().remove(imageFragment).commit();
+                default:
+                    break;
+            }
+        }
     }
 }
